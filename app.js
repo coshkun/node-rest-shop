07926f3d3a73bd063d8bf65aff9d343aa155c358
Coskun Caner <coskun@Eda-MacBook-Pro.local>
2020-02-01 01:19:31 +0300

    Error handling & Request logger added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
 const productRoutes = require('./api/routes/product')
 const ordersRoutes = require('./api/routes/orders')
@@ -8,11 +10,27 @@ app.use('/products', productRoutes)
 app.use('/orders', ordersRoutes)
 
 
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         err: 0,
+//         message: "It works!"
+//     })
+// });
+
+//Error Handler for all other incoming requests
 app.use((req, res, next) => {
-    res.status(200).json({
-        err: 0,
-        message: "It works!"
+    const error = Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+//Error Handler for all other thrown errors from application
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        err: 1,
+        message: error.message
     })
-});
+})
 
 module.exports = app;
