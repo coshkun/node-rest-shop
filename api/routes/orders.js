1eba0db46cc8b17f4742046154e0ff5fd4c620ea
Coskun Caner <cskncnr@gmail.com>
2020-02-11 03:55:56 +0300

    Orders & un-populated Products

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,44 +1,126 @@
 const express = require('express')
 const router = express.Router()
+const baseUrl = process.env.BASE_URL || 'http://localhost'
+const PORT = process.env.PORT || '3000'
+
+const mongoose = require('mongoose')
+const Order = require('../models/order')
+const Product = require('../models/product')
 
 //MARK: - List Products
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        err: 0,
-        message: "No orders awailible yet."
+
+    Order.find()
+    .select('_id product quantity')
+    .exec()
+    .then(docs => {
+        console.log(docs)
+        res.status(200).json({
+            err: 0,
+            message: "success",
+            count: docs.length,
+            response: docs.map(doc => {
+                return {
+                    id: doc._id,
+                    quantity: doc.quantity,
+                    productId: doc.product,
+                    request: {
+                        type: 'GET',
+                        url: `${baseUrl}:${PORT}/orders/${doc._id}`
+                    }
+                }
+            })
+        })
     })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            err: 1,
+            message: "Unable to find orders.",
+            response: err
+        })
+    });
 })
 
 //MARK: - Create Single
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    }
-
-    res.status(201).json({
-        err: 0,
-        message: "No order created yet.",
-        request: order,
-        response: {}
+    // Check if we have the product in DB
+    Product.findById(req.body.productId)
+    .then(product => {
+        if(!product) {
+            return res.status(404).json({
+                err: 1,
+                message: "Product not foud, can not create order.",
+                request: req.body,
+                response: {}
+            })
+        }
+        const order = new Order({
+            _id: mongoose.Types.ObjectId(),
+            quantity: req.body.quantity,
+            product: req.body.productId
+        });
+    
+        return order.save()
     })
+    .then(result => {
+        console.log(result)
+        res.status(201).json({
+            err: 0,
+            message: "Order created.",
+            response: {
+                id: result._id,
+                productId: result.product,
+                quantity: result.quantity
+            },
+            request: {
+                type: 'GET',
+                url: `${baseUrl}:${PORT}/orders/${result._id}`
+            }
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            err: 1,
+            message: "Can not create order.",
+            request: req.body,
+            response: err
+        })
+    });
 })
 
 //MARK: - Get Detail By ID
 router.get('/:orderId',(req, res, next) => {
-    const id = req.params.orderId
-    if (id === 'special') {
+    Order.findById(req.params.orderId)
+    .exec()
+    .then(order => {
+        console.log(order)
+        if (!order) {
+            return res.status(404).json({
+                err: 1,
+                message: "Order not found"
+            })
+        }
+        
         res.status(200).json({
             err: 0,
-            message: "You have reached the special order's details.",
-            id: id
+            message: "success",
+            response: order,
+            request: {
+                type: 'GET',
+                url: `${baseUrl}:${PORT}/orders`
+            }
         })
-    } else {
-        res.status(200).json({
-            err: 0,
-            message: "You have passed an id: " + id
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            err: 1,
+            message: "fails",
+            response: err
         })
-    }
+    });
 })
 
 //MARK: - Update Single By ID
@@ -52,11 +134,27 @@ router.patch('/:orderId',(req, res, next) => {
 
 //MARK: - Delete Single By ID
 router.delete('/:orderId',(req, res, next) => {
-    const id = req.params.orderId
-    res.status(200).json({
-        err: 0,
-        message: "Deleted order id: " + id
+    Order.remove({ _id: req.params.orderId })
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            err: 0,
+            message: "Order deleted.",
+            request: {
+                type: 'POST',
+                url: `${baseUrl}:${PORT}/orders`,
+                body: { productId: 'STRING', quantity: 'INT' }
+            }
+        })
     })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            err: 1,
+            message: "fails",
+            response: err
+        })
+    });
 })
 
 module.exports = router
